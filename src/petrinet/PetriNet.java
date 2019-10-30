78014247268496c701b0e4e4440f7bfb90ec1053
sienmich <sienmich@gmail.com>
2019-10-30 16:16:03 +0100

    Implemented using wait and notify.

--- src/petrinet/PetriNet.java
+++ src/petrinet/PetriNet.java
@@ -1,24 +1,56 @@
 package petrinet;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-...
+import java.util.Stack;
 
 public class PetriNet<T> {
 
+	private Map<T, Integer> state;
+	boolean fair;
+	
+
     public PetriNet(Map<T, Integer> initial, boolean fair) {
-        ...
-    }
+		this.state = initial;
+		this.fair = fair;
+	}
 
     public Set<Map<T, Integer>> reachable(Collection<Transition<T>> transitions) {
-        ...
+    	Set<Map<T, Integer>> res = new HashSet<Map<T, Integer>>();
+    	Stack<Map<T, Integer>> toCheck = new Stack<Map<T, Integer>>();
+    	
+    	toCheck.add(state);
+    	
+    	while(!toCheck.empty()) {
+    		Map<T, Integer> newState = toCheck.pop();
+    		if(res.add(newState)) {
+	    		for(Transition<T> t : transitions)
+	    			if(t.canTransit(newState)) {
+	    				Map<T, Integer> copy = new HashMap<>(newState);
+	    				t.transit(copy);
+	    				toCheck.add(copy);
+	    			}
+    		}
+    	}
+    	
+    	return res;
     }
 
-    public Transition<T> fire(Collection<Transition<T>> transitions) throws InterruptedException {
-        ...
+    public synchronized Transition<T> fire(Collection<Transition<T>> transitions) throws InterruptedException {
+    	while(true) {
+        	for(Transition<T> t : transitions) {
+        		if(t.canTransit(state)) {
+        			t.transit(state);
+        			notifyAll();
+        			return t;
+        		}
+        	}
+        	wait();
+    	}
     }
 
-    ...
 
 }
